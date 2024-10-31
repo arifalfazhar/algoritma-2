3fb8bb8cbb5d00690e071451fd6185fed8d0ab6a
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:43:26 -0700

    20 tipe data.php

---
+++ 20 tipe data.php
@@ -0,0 +1,28 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+// tipe data string (teks)
+$nama_lengkap = "arif alfazhar";
+
+// tipe data integer
+$umur = 15;
+
+// tipe data float
+$berat = 42.3;
+
+// tipe data float
+$tinggi = 163.2;
+
+// tipe data boolean
+$remaja = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "remaja: $remaja";
+
+echo "<i><br><br><strong>By :arif alfazhar</strong=";
