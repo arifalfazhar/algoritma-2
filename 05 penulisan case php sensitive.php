bd4f583a2ad10f9cf363f33b4ff083e03294413b
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:16:29 -0700

    05 penulisan case php sensitive

---
+++ 05 penulisan case php sensitive.php
@@ -0,0 +1,23 @@
+<?php
+
+$nama = "arif alfazhar";
+$NAMA = "iqbal";
+$umur = 15;
+const INI_KONSTANTA = 123;
+$ini_variabel = 23;
+$iniJugaVariabel = 49;
+
+
+echo " ==== mencetak variabel ==== ";
+echo "<p>Nama saya adalah $nama";
+echo "<p>Nama seketaris adalah $NAMA";
+echo "<p>Umur saya adalah $umur";
+
+echo 
+
+    "<p>coba pakai blok program",
+    "<p>Nama saya adalah $nama",
+    "<p>Nama seketaris adalah $NAMA",
+    "<p>Umur saya adalah $umur";
+
+?>
