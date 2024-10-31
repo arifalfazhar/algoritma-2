f9a0dbaabc071de8dfe6e8dc1c027b98921565c6
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:40:13 -0700

    17 tipe data float.php

---
+++ 17 tipe data float.php
@@ -0,0 +1,8 @@
+<?php
+$waktu = 190.12;
+$kecepatan = 13243.12;
+$jarak = 230.123 * 13266.12 ;
+
+// batasi angka di belakang koma
+echo "<p>nilai jarak: $jarak";
+echo "<i><p><strong>By :arif alfazhar</strong=";
