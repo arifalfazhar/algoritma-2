32d3766fd8dc498870ab69260dd58a1e88bb879f
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:52:45 -0700

    25 konversi data di php.php

---
+++ 25 konversi data di php.php
@@ -0,0 +1,7 @@
+<?php
+$var = "1234567.89";
+$intvar = "123456789";
+$intvar = intval($var);//mengubah menjadi integer
+$floatvar = floatval($var);// mengubah menjadi float
+echo $intvar;//output:123456789
+echo "<p>$floatvar";//output:123.456789
