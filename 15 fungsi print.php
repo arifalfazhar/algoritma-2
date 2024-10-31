20698951d1293c5d8646c03bf1bd82db9131c6c7
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:37:17 -0700

    15 fungsi print.php

---
+++ 15 fungsi print.php
@@ -0,0 +1,11 @@
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 500000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("<p>Harganya adalah Rp %.2f", $harga);
+
+echo "<i><br><br><br><strong>By : arif alfazhar</strong>";
