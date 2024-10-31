a5a287b551019eb9031f3c6113ea51243bea197e
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:44:32 -0700

    21 Tipe data Boolean di PHP.php

---
+++ 21 Tipe data Boolean di PHP.php
@@ -0,0 +1,19 @@
+<?php
+//mendeklarasikan variable boolean 
+$isActive = false;
+$menikah = true;
+//menggunakan boolean dalam peryataan if
+if ($isActive){
+    echo "Nilai ini benar.";
+} else{
+    echo "Nilai ini salah.";
+}
+
+//contoh lain mnggunakan boolean 
+$condition = (5 > 2);
+if ($condition) {
+    echo "<p>4 lebih besar dari 2.";
+} else {
+    echo "4 tidak lebih besar dari 2.";
+}
+echo "<i><br><br><br><strong>By  :arif alfazhar</strong>";
