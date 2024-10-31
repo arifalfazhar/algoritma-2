11eadd1ac13364436d14c3153d014baca6e41418
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:42:29 -0700

    19 tipe data intiger.php

---
+++ 19 tipe data intiger.php
@@ -0,0 +1,8 @@
+<?php
+$nilai = 76; // angka positif
+$poin = -19; // angka negatif
+
+echo "angka positif: $nilai";
+echo "<p>angka negatif: $poin";
+
+echo "<i><p><strong>By :arif alfazhar</strong=";
