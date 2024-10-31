23b83514cb5a3cb9e72ffece22823ed5fe5886e3
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:34:45 -0700

    14 mengabungkan string atau teks di php.php

---
+++ 14 mengabungkan string atau teks di php.php
@@ -0,0 +1,11 @@
+<?php
+$txt1 = "cara belajar php";
+$txt2 = "arif alfazhar";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Dibelajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+echo "<i><p><strong>By    : arif alfazhar"
+?>
