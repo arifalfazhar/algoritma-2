d2e90c33742529a7985315eb1fd2188ed633b11b
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:26:29 -0700

    09 konstanta.php

---
+++ 09 konstanta.php
@@ -0,0 +1,29 @@
+<?php
+// membuat konstanta dengan fungsi define()
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'arif alfazhar');
+define('DB_PASS', 'terbaru');
+define('DB_KEGIATAN', 'ngoding');
+define('DB_API_KEY', '1234');
+
+// membuat konstanta dengan kata kunci const
+const API_KEY = "1234";
+
+const SITE_NAME = "arif alfazhar";
+const SITE_LOCAL = "terbaru";
+const SITE_RAHASIA = "ngoding";
+const SITE_BELAJAR = "ngoding";
+const SITE_API_KEY = "1234";
+
+
+echo "Nama User: " . SITE_NAME;
+echo "<p>Nama Server: " . SITE_LOCAL;
+echo "<p>Nama Pass: " . SITE_RAHASIA;
+echo "<p>Nama Kegiatan: " . SITE_BELAJAR;
+echo "<p>Nama Apikey: " . SITE_API_KEY;
+
+
+
+
+
+echo "<i><br><br><br><strong>By : arif alfazhar</strong>";
