cfe7a795fe0eaa0d8568cd1f632813e14b477a3e
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:08:18 -0700

    03 html dalam php

---
+++ 3 html dalam php.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<title>Judul Web</title>";
+echo "</head>";
+echo "<body>";
+echo "<p><h2>Selamat datang</h2><p>";
+echo "<p>DI SMKN 1 KARANG BARUU</p>";
+echo "<p>BY     : ARIF ALFAZHAR</p>";
+echo "</body>";
+echo "</html>";
