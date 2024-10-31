c255d8573e88b3bcde68860596f949ea55e8a36c
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:28:15 -0700

    10 site name.php

---
+++ 10 site name.php
@@ -0,0 +1,13 @@
+<?php // file: belajar-konstanta.php
+
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME  = "arif alfazhar";
+const BASE_URL    = "https://www.arif alfazhar";
+
+// cetak nilai konstanta
+echo "Site name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
+Hasilnya:
