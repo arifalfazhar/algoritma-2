8cc5b573e54f6691d0e80b118959a10cdc249974
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:31:27 -0700

    12 fungsi echo.php

---
+++ 12 fungsi echo.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo("Hello world!<br>");
+echo "Belajar ngoding bang!<br>";
+echo "Biar ", "pandai ", "dan ", "mudah belajar ", "php.";
+echo "<i><p><strong>By    : arif alfazhar"
+?>
