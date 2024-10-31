4ff3770d6ef1e7dc63edaddf85ec063db5d1c0a3
arifalfazhar <rokokksp03@gmail.com>
2024-10-31 02:30:01 -0700

    11 cetak nilai variabel baru.php

---
+++ 11 cetak nilai variabel baru.php
@@ -0,0 +1,10 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "rx king";
+$harga = 15000;
+
+// menampilkan isi variabel
+echo "saya membeli $nama_barang seharga Rp $harga";
+
+echo "<i><br><br><br><strong>By  : arif alfazhar</strong>";
